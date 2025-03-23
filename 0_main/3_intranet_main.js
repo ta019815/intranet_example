dfaf761661b9128cd242e8f4789e098640c9ba6a
one_b02 <one_b02@naver.com>
2025-03-23 20:14:07 +0900

    메인 페이지 재구축

--- 0_main/3_intranet_main.js
+++
@@ -1,6 +0,0 @@
-// // 로그아웃 버튼 클릭 시
-// document.querySelector('.logout-button').addEventListener('click', function() {
-//     // 로그아웃 후 새로운 페이지로 이동 (뒤로가기 방지)
-//     location.replace('로그아웃후_이동할_페이지.html'); // 여기에 이동할 페이지 URL 입력
-//   });
-  
